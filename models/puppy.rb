573aaf33690306c826329a5cb01dd4f4cf609e73
Jed Peek <jedpeek@yahoo.com>
2018-07-24 04:44:45 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,8 @@
 class Puppy
-
-  def initialize
-    @puppy = puppy
+attr_accessor :name, :breed, :age
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
 end
